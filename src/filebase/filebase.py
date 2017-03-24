25954035f9ef4e395612fe104511666d7308a455
Diego Rodrigues <diego.mrodrigues@outlook.com>
2017-03-24 17:59:34 -0300

    Cleanup

--- src/filebase/filebase.py
+++ src/filebase/filebase.py
@@ -1,9 +1,7 @@
 import os
 import cPickle as pickle
 
-
 class Fb_query(object):
-
 	path_default = "/var/www/html/dev/fbM2M/fb/"
 
 	def __init__(self, collection):
@@ -13,35 +11,26 @@ class Fb_query(object):
 
 	# inseri arquivo no direfotio informado
 	# [parametros]
-
-
 	def created(self, index, value):
-		
-
 		if self.created_path(self.collection) :
-
 			file = open(self.collection + "/" + index + ".bin", "w")
 			pickle.dump(value, file)
 			file.close()
 			return True
 
 	def read(self, index):
-
 		_file = self.collection + "/" + index + ".bin"
 		if os.path.isfile(_file):
 			file = open(_file)
 			unFile = pickle.load(file)
 			file.close()
 			return unFile
-		else :
+		else:
 			return False
-		
-
 
 	def created_path(self, path):
-
 		if os.path.exists(path):
 			return True
-		else :
+		else:
 			os.makedirs(path)
 			return True
